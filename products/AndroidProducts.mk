538570c8bb9f4f0e0674a71cc756d25060ab93d1
Hokyu Kim <dogelabshokyu@gmail.com>
2021-08-02 21:36:16 +0900

    products: Add Pantech VEGA IRON 2 (ef63)

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -23,6 +23,7 @@ PRODUCT_MAKEFILES += \
     $(LOCAL_DIR)/daisy/pa_daisy.mk \
     $(LOCAL_DIR)/davinci/pa_davinci.mk \
     $(LOCAL_DIR)/dipper/pa_dipper.mk \
+    $(LOCAL_DIR)/ef63/pa_ef63.mk \
     $(LOCAL_DIR)/equuleus/pa_equuleus.mk \
     $(LOCAL_DIR)/ginkgo/pa_ginkgo.mk \
     $(LOCAL_DIR)/hlte/pa_hlte.mk \
@@ -64,6 +65,7 @@ COMMON_LUNCH_CHOICES += \
     pa_daisy-userdebug \
     pa_davinci-userdebug \
     pa_dipper-userdebug \
+    pa_ef63-userdebug \
     pa_equuleus-userdebug \
     pa_ginkgo-userdebug \
     pa_hlte-userdebug \
